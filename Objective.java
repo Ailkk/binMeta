05c1b58b67860cb56fe9e6a5b709c812d762bbf0
Thomas-Guibert <thomas.guibert0930@gmail.com>
2020-11-09 14:27:04 +0100

    Commentaire Objectif

--- Objective.java
+++ Objective.java
@@ -7,25 +7,48 @@
  *
  * AM
  */
-
+/**
+ * 
+ * @author Guibert Thomas
+ *
+ * Classe qui défini un Objectif d'un algo heuristique et qui affiche son evaluation si elle
+ *  a été faite, elle affiche "objective was not evaluated yet" si aucune evaluation n'a ete faite 
+ *  (C'est seulement la dernière evalution qui est donner, c'est la solution courante)
+ * 
+ */
 public abstract class Objective
 {
+	/**
+	 * Nom de l'objectif ainsi que la valeur de la derniere evaluation
+	 */
    // protected attributes
    protected String name;
    protected Double lastValue = null;
 
+   /**
+    *Donne le nom de l'objectif
+    */
    // getter for objective name
    public String getName()
    {
       return this.name;
    }
 
+   /**
+    *Prend des possible solution de manière aléatoire
+    */
    // abstract method "solutionSample" (possibly a random sample)
    public abstract Data solutionSample();
 
+   /**
+    *Value l'objectif
+    */
    // abstract method "value"
    public abstract double value(Data D);
 
+   /**
+    *Affichage de la derniere solution si il y en a une
+    */
    // toString
    public String toString()
    {
