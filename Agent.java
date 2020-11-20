f9048b51e8f073c456dcab21b4167956b7e6a3cd
Ailk <ailk@Romain.localdomain>
2020-11-20 23:17:35 +0100

    work on GSA

--- Agent.java
+++ Agent.java
@@ -1,19 +1,73 @@
-
+//normalement on a plus besoin de cette classe
 public class Agent {
-
-	Data x, y, z;
-	Data masse;
-	Data vitesse;
-	Data acceleration;
+/*
+	// les 3 coordonnées dans un espace 3D
+	private Data x, y, z;
 	
-	public Agent(Data x, Data y, Data z, Data masse, Data vitesse, Data acceleration) {
+	private Data masse;
+	private Data vitesse;
+	private Data acceleration;
+	
+	public Agent(int x, int y, int z, int masse, int vitesse, int acceleration) {
+		
+		this.x = new Data(x);
+		this.y = new Data(y);
+		this.z = new Data(z);
+		this.masse = new Data(masse);
+		this.vitesse = new Data(vitesse);
+		this.acceleration = new Data(acceleration);
+		
+	}
+
+	public Data getX() {
+		return x;
+	}
+
+	public void setX(Data x) {
 		this.x = x;
+	}
+
+	public Data getY() {
+		return y;
+	}
+
+	public void setY(Data y) {
 		this.y = y;
+	}
+
+	public Data getZ() {
+		return z;
+	}
+
+	public void setZ(Data z) {
 		this.z = z;
-		this.masse = masse;
+	}
+
+	public Data getMasse() {
+		return masse;
+	}
+
+	
+	//Pas besoin de changer la masse d'un agent
+
+	public Data getVitesse() {
+		return vitesse;
+	}
+
+	public void setVitesse(Data vitesse) {
 		this.vitesse = vitesse;
+	}
+
+	public Data getAcceleration() {
+		return acceleration;
+	}
+
+	public void setAcceleration(Data acceleration) {
 		this.acceleration = acceleration;
 	}
 	
+	
 	//TODO
+	 
+	*/
 }
