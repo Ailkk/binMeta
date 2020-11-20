4eca68dda483d43df9b917b6efbcbe6fbac09113
Ailk <ailk@Romain.localdomain>
2020-11-20 21:03:06 +0100

    ajout classe agent

---
+++ Agent.java
@@ -0,0 +1,19 @@
+
+public class Agent {
+
+	Data x, y, z;
+	Data masse;
+	Data vitesse;
+	Data acceleration;
+	
+	public Agent(Data x, Data y, Data z, Data masse, Data vitesse, Data acceleration) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.masse = masse;
+		this.vitesse = vitesse;
+		this.acceleration = acceleration;
+	}
+	
+	//TODO
+}
